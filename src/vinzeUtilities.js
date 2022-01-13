10c8724ad1396f1a357c0f2bf8e8c7434660b4e3
Patryk Surmacz <patryk@p88.pl>
2022-01-13 15:18:42 +0100

    Code refactoring

--- src/vinzeUtilities.js
+++ src/vinzeUtilities.js
@@ -3,14 +3,16 @@
 /* created by Patryk Surmacz */
 /* github.com/vinze-ps */
 Object.defineProperty(exports, "__esModule", { value: true });
-class Vinze {
-    constructor() {
-        this.isMobileDevice = () => (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-        this.doInSomeTime = (since, to, fn, elsefn = null) => {
-            let date = new Date();
-            let sinceNumber = +(since.split(":")[0] + since.split(":")[1]);
-            let toNumber = +(to.split(":")[0] + to.split(":")[1]);
-            let currentTimeNumber = +(date.getHours().toString() + (date.getMinutes() < 10 ? "0" : "") + date.getMinutes().toString());
+var Vinze = /** @class */ (function () {
+    function Vinze() {
+        var _this = this;
+        this.isMobileDevice = function () { return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)); };
+        this.doInSomeTime = function (since, to, fn, elsefn) {
+            if (elsefn === void 0) { elsefn = null; }
+            var date = new Date();
+            var sinceNumber = +(since.split(":")[0] + since.split(":")[1]);
+            var toNumber = +(to.split(":")[0] + to.split(":")[1]);
+            var currentTimeNumber = +(date.getHours().toString() + (date.getMinutes() < 10 ? "0" : "") + date.getMinutes().toString());
             if (sinceNumber < toNumber) {
                 if (currentTimeNumber >= sinceNumber && currentTimeNumber <= toNumber) {
                     fn();
@@ -35,23 +37,26 @@ class Vinze {
                 fn();
             }
         };
-        this.timeout = (action, name = null, duration = 0, fn = null) => {
+        this.timeout = function (action, name, duration, fn) {
+            if (name === void 0) { name = null; }
+            if (duration === void 0) { duration = 0; }
+            if (fn === void 0) { fn = null; }
             switch (action) {
                 case "set":
-                    let indexOfTimeout = -1;
-                    if (this.timeouts.length > 0) {
-                        for (let i = 0; i <= this.timeouts.length - 1; i++) {
-                            indexOfTimeout = this.timeouts[i].indexOf(name);
+                    var indexOfTimeout = -1;
+                    if (_this.timeouts.length > 0) {
+                        for (var i = 0; i <= _this.timeouts.length - 1; i++) {
+                            indexOfTimeout = _this.timeouts[i].indexOf(name);
                             if (indexOfTimeout !== -1) {
-                                this.timeouts[i][1] = setTimeout(() => {
+                                _this.timeouts[i][1] = setTimeout(function () {
                                     if (fn)
                                         fn();
                                 }, duration);
                             }
-                            else if (indexOfTimeout === -1 && i === this.timeouts.length - 1) {
-                                this.timeouts.push([
+                            else if (indexOfTimeout === -1 && i === _this.timeouts.length - 1) {
+                                _this.timeouts.push([
                                     name,
-                                    setTimeout(() => {
+                                    setTimeout(function () {
                                         if (fn)
                                             fn();
                                     }, duration),
@@ -60,9 +65,9 @@ class Vinze {
                         }
                     }
                     else {
-                        this.timeouts.push([
+                        _this.timeouts.push([
                             name,
-                            setTimeout(() => {
+                            setTimeout(function () {
                                 if (fn)
                                     fn();
                             }, duration),
@@ -70,44 +75,47 @@ class Vinze {
                     }
                     return indexOfTimeout;
                 case "isset":
-                    for (let i = 0; i <= this.timeouts.length - 1; i++)
-                        if (this.timeouts[i][0] === name)
+                    for (var i = 0; i <= _this.timeouts.length - 1; i++)
+                        if (_this.timeouts[i][0] === name)
                             return true;
                     return false;
                 case "clear":
-                    for (let i = 0; i <= this.timeouts.length - 1; i++)
-                        if (this.timeouts[i][0] === name) {
-                            clearTimeout(this.timeouts[i][1]);
-                            this.timeouts.splice(i, 1);
+                    for (var i = 0; i <= _this.timeouts.length - 1; i++)
+                        if (_this.timeouts[i][0] === name) {
+                            clearTimeout(_this.timeouts[i][1]);
+                            _this.timeouts.splice(i, 1);
                         }
                     return -1;
                 case "clearAll":
-                    for (let i = 0; i <= this.timeouts.length - 1; i++) {
-                        clearTimeout(this.timeouts[i][1]);
-                        this.timeouts = new Array();
+                    for (var i = 0; i <= _this.timeouts.length - 1; i++) {
+                        clearTimeout(_this.timeouts[i][1]);
+                        _this.timeouts = new Array();
                     }
                     return -1;
                 default:
                     return -1;
             }
         };
-        this.interval = (action, name = null, tick = 0, fn = null) => {
+        this.interval = function (action, name, tick, fn) {
+            if (name === void 0) { name = null; }
+            if (tick === void 0) { tick = 0; }
+            if (fn === void 0) { fn = null; }
             switch (action) {
                 case "set":
-                    let indexOfInterval = -1;
-                    if (this.intervals.length > 0) {
-                        for (let i = 0; i <= this.intervals.length - 1; i++) {
-                            indexOfInterval = this.intervals[i].indexOf(name);
+                    var indexOfInterval = -1;
+                    if (_this.intervals.length > 0) {
+                        for (var i = 0; i <= _this.intervals.length - 1; i++) {
+                            indexOfInterval = _this.intervals[i].indexOf(name);
                             if (indexOfInterval !== -1) {
-                                this.intervals[i][1] = setTimeout(() => {
+                                _this.intervals[i][1] = setTimeout(function () {
                                     if (fn)
                                         fn();
                                 }, tick);
                             }
-                            else if (indexOfInterval === -1 && i === this.intervals.length - 1) {
-                                this.intervals.push([
+                            else if (indexOfInterval === -1 && i === _this.intervals.length - 1) {
+                                _this.intervals.push([
                                     name,
-                                    setTimeout(() => {
+                                    setTimeout(function () {
                                         if (fn)
                                             fn();
                                     }, tick),
@@ -116,9 +124,9 @@ class Vinze {
                         }
                     }
                     else {
-                        this.intervals.push([
+                        _this.intervals.push([
                             name,
-                            setTimeout(() => {
+                            setTimeout(function () {
                                 if (fn)
                                     fn();
                             }, tick),
@@ -126,21 +134,21 @@ class Vinze {
                     }
                     return indexOfInterval;
                 case "isset":
-                    for (let i = 0; i <= this.intervals.length - 1; i++)
-                        if (this.intervals[i][0] === name)
+                    for (var i = 0; i <= _this.intervals.length - 1; i++)
+                        if (_this.intervals[i][0] === name)
                             return true;
                     return false;
                 case "clear":
-                    for (let i = 0; i <= this.intervals.length - 1; i++)
-                        if (this.intervals[i][0] === name) {
-                            clearInterval(this.intervals[i][1]);
-                            this.intervals.splice(i, 1);
+                    for (var i = 0; i <= _this.intervals.length - 1; i++)
+                        if (_this.intervals[i][0] === name) {
+                            clearInterval(_this.intervals[i][1]);
+                            _this.intervals.splice(i, 1);
                         }
                     return -1;
                 case "clearAll":
-                    for (let i = 0; i <= this.intervals.length - 1; i++) {
-                        clearInterval(this.intervals[i][1]);
-                        this.intervals = new Array();
+                    for (var i = 0; i <= _this.intervals.length - 1; i++) {
+                        clearInterval(_this.intervals[i][1]);
+                        _this.intervals = new Array();
                     }
                     return -1;
                 default:
@@ -150,23 +158,30 @@ class Vinze {
         this.timeouts = [];
         this.intervals = [];
     }
-    objectAssign(target, ...sources) {
-        sources.forEach((source) => {
-            Object.keys(source).forEach((key) => {
-                const s_val = source[key];
-                const t_val = target[key];
-                target[key] = t_val && s_val && typeof t_val === "object" && typeof s_val === "object" ? this.objectAssign(t_val, s_val) : s_val;
+    Vinze.prototype.objectAssign = function (target) {
+        var _this = this;
+        var sources = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            sources[_i - 1] = arguments[_i];
+        }
+        sources.forEach(function (source) {
+            Object.keys(source).forEach(function (key) {
+                var s_val = source[key];
+                var t_val = target[key];
+                target[key] = t_val && s_val && typeof t_val === "object" && typeof s_val === "object" ? _this.objectAssign(t_val, s_val) : s_val;
             });
         });
         return target;
-    }
-    throttle(name, func, delay) {
+    };
+    Vinze.prototype.throttle = function (name, func, delay) {
+        var _this = this;
         if (this.timeout("isset", name))
             return;
         func();
-        this.timeout("set", name, delay, () => {
-            this.timeout("clear", name);
+        this.timeout("set", name, delay, function () {
+            _this.timeout("clear", name);
         });
-    }
-}
+    };
+    return Vinze;
+}());
 exports.default = Vinze;
